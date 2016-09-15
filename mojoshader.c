4b1c832151d2815a698a384183d8cadfffa14416
Melker Narikka <meklu@meklu.org>
2016-09-15 21:29:34 +0300

    I seem to have dropped this

--- mojoshader.c
+++ mojoshader.c
@@ -8653,7 +8653,7 @@ static ComponentList *componentlist_alloc(Context *ctx)
     ret->v.i = 0;
     ret->next = NULL;
     return ret;
-}
+} // componentlist_alloc
 
 static const char *get_SPIRV_varname_in_buf(Context *ctx, const RegisterType rt,
                                            const int regnum, char *buf,
